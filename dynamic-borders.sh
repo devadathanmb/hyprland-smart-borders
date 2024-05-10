34ac148214b59eea3a2e321a2e66d002d6b0655b
Devadathan M B <devadathanmb@gmail.com>
2024-05-10 10:50:32 +0530

    feat: change socket directory

--- dynamic-borders.sh
+++ dynamic-borders.sh
@@ -115,4 +115,6 @@ function handle {
     fi
 }
 
-socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
+# Socket directory has changed in Hyprland v0.40.0
+# socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
+socat - UNIX-CONNECT:$(echo $XDG_RUNTIME_DIR)/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do handle $line; done
